5bb6b1679f43fb5db72f8c8ab6d75138ed447810
Administrator <Administrator@User-2022ZTPHFK>
2022-07-22 17:51:43 +0900

    0722-green

---
+++ src/main/java/com/project/jongin/domain/dto/board/BoardDetailDTO.java
@@ -0,0 +1,54 @@
+package com.project.jongin.domain.dto.board;
+
+import java.util.Set;
+
+import com.project.jongin.domain.entity.BoardEntity;
+import com.project.jongin.domain.enumes.BuildType;
+import com.project.jongin.domain.enumes.HouseType;
+import com.project.jongin.domain.enumes.OptionType;
+import com.project.jongin.domain.enumes.PayType;
+
+import lombok.Getter;
+	
+@Getter
+public class BoardDetailDTO {
+	private long boardNo;
+	private HouseType boardHouseType;
+	private BuildType boardBuildType;
+	private String boardAddress;
+	private String boardAddressDetail;
+	private PayType boardPayType;
+	private int boardDeposit;
+	private int boardPaymonth;
+	private int boardSupply;
+	private int boardExclusive;
+	private int boardAllFloors;
+	private int boardPositonFloor;
+	private String boardInDate;
+	private Set<OptionType> boardOption;
+	private String boardTitle;
+	private String boardContents;
+	private String boardHiddenMemo;
+	private String memberEmail;
+	
+	public BoardDetailDTO(BoardEntity e) {
+		boardNo=e.getBoardNo();
+		boardHouseType=e.getBoardHouseType();
+		boardBuildType=e.getBoardBuildType();
+		boardAddress=e.getBoardAddress();
+		boardAddressDetail=e.getBoardAddressDetail();
+		boardPayType=e.getBoardPayType();
+		boardDeposit=e.getBoardDeposit();
+		boardPaymonth=e.getBoardPaymonth();
+		boardSupply=e.getBoardSupply();
+		boardExclusive=e.getBoardExclusive();
+		boardAllFloors=e.getBoardAllFloors();
+		boardPositonFloor=e.getBoardPositonFloor();
+		boardInDate=e.getBoardInDate();
+		boardOption=e.getBoardOption();
+		boardTitle=e.getBoardTitle();
+		boardContents=e.getBoardContents();
+		boardHiddenMemo=e.getBoardHiddenMemo();
+		memberEmail=e.getMemberEntity().getMemberEmail();
+	}
+}
---
+++ src/main/java/com/project/jongin/domain/dto/board/BoardListDTO.java
@@ -0,0 +1,25 @@
+package com.project.jongin.domain.dto.board;
+
+import com.project.jongin.domain.entity.BoardEntity;
+import com.project.jongin.domain.enumes.PayType;
+
+import lombok.Getter;
+
+@Getter
+public class BoardListDTO {
+	private long boardNo;
+	private String boardTitle;
+	private String boardWriter;
+	private int boardPaymonth;
+	private PayType boardPayType;
+	private String boardAddress;
+	
+	public BoardListDTO(BoardEntity e) {
+		boardNo=e.getBoardNo();
+		boardTitle=e.getBoardTitle();
+		boardWriter=e.getMemberEntity().getMemberEmail();
+		boardPaymonth=e.getBoardPaymonth();
+		boardPayType=e.getBoardPayType();
+		boardAddress=e.getBoardAddress();
+	}
+}
